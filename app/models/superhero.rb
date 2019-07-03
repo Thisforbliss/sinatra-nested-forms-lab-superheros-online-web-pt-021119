928db7991aafbb433edf545c645fbd2353ac5657
Malcolm Leake <echelon1089@gmail.com>
2019-07-03 22:57:14 +0000

    Automatically backed up by Learn

---
+++ app/models/superhero.rb
@@ -0,0 +1,16 @@
+class Superhero
+  attr_accessor :name, :power, :biography
+
+  @@all = []
+
+  def initialize(name, power, biography)
+    @name = name
+    @power = power
+    @biography = biography
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+end
